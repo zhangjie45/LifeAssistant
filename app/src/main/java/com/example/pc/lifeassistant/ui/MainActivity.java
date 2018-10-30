c7ac748890483807085ad5aeca885f4d99160408
zhangjie <1109446910@qq.com>
2018-10-30 19:50:58 +0800

    资源文件上传

--- app/src/main/java/com/example/pc/lifeassistant/ui/MainActivity.java
+++ app/src/main/java/com/example/pc/lifeassistant/ui/MainActivity.java
@@ -1,10 +1,8 @@
 package com.example.pc.lifeassistant.ui;
 
-import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.widget.Toast;
 
 import com.ashokvarma.bottomnavigation.BottomNavigationBar;
 import com.ashokvarma.bottomnavigation.BottomNavigationItem;
@@ -36,15 +34,15 @@ public class MainActivity extends Utils implements BottomNavigationBar.OnTabSele
         bottomNavigationBar.setBackgroundStyle(BottomNavigationBar.BACKGROUND_STYLE_STATIC);
         bottomNavigationBar.setMode(BottomNavigationBar.MODE_FIXED);
         bottomNavigationBar.setActiveColor(R.color.colorAccent);//值得一提，模式跟背景的设置都要在添加tab前面，不然不会有效果。
-        bottomNavigationBar.addItem(new BottomNavigationItem(R.drawable.home_fill, getString(R.string.item_home)).setInactiveIconResource(R.drawable.home).setActiveColorResource(R.color.colorPrimary).setInActiveColorResource(R.color.black_1))
-                .addItem(new BottomNavigationItem(R.drawable.location_fill, getString(R.string.item_location)).setInactiveIconResource(R.drawable.location).setActiveColorResource(R.color.colorPrimary).setInActiveColorResource(R.color.black_1))
-                .addItem(new BottomNavigationItem(R.drawable.like_fill, getString(R.string.item_like)).setInactiveIconResource(R.drawable.like).setActiveColorResource(R.color.colorPrimary).setInActiveColorResource(R.color.black_1))
-                .addItem(new BottomNavigationItem(R.drawable.person_fill, getString(R.string.item_person)).setInactiveIconResource(R.drawable.person).setActiveColorResource(R.color.colorPrimary).setInActiveColorResource(R.color.black_1))
+        bottomNavigationBar.addItem(new BottomNavigationItem(R.drawable.home_fill, getString(R.string.item_home)).setInactiveIconResource(R.drawable.home).setActiveColorResource(R.color.bottom_navigation_item_color).setInActiveColorResource(R.color.black_1))
+                .addItem(new BottomNavigationItem(R.drawable.date_fill, getString(R.string.item_date)).setInactiveIconResource(R.drawable.date).setActiveColorResource(R.color.bottom_navigation_item_color).setInActiveColorResource(R.color.black_1))
+                .addItem(new BottomNavigationItem(R.drawable.capital_fill, getString(R.string.item_capital)).setInactiveIconResource(R.drawable.capital).setActiveColorResource(R.color.bottom_navigation_item_color).setInActiveColorResource(R.color.black_1))
+                .addItem(new BottomNavigationItem(R.drawable.setting_fill, getString(R.string.item_setting)).setInactiveIconResource(R.drawable.setting).setActiveColorResource(R.color.bottom_navigation_item_color).setInActiveColorResource(R.color.black_1))
                 .setFirstSelectedPosition(0)
                 .initialise();
         bottomNavigationBar.setTabSelectedListener(this);
         initToolbar(R.id.tl_, R.id.toolbar_title, "首页");
-            setDefaultFragment();
+        setDefaultFragment();
     }
 
     private void setDefaultFragment() {
@@ -55,10 +53,9 @@ public class MainActivity extends Utils implements BottomNavigationBar.OnTabSele
     }
 
     public void init() {
-        bottomNavigationBar = (BottomNavigationBar) findViewById(R.id.bottom_nar_bar);
+        bottomNavigationBar = findViewById(R.id.bottom_nar_bar);
 
     }
-
     @Override
     public void onTabSelected(int position) {
         FragmentManager fm = getFragmentManager();
@@ -67,15 +64,14 @@ public class MainActivity extends Utils implements BottomNavigationBar.OnTabSele
             case 0:
                 initToolbar(R.id.tl_, R.id.toolbar_title, "首页");
                 if (homeFragment == null) {
-                    homeFragment = HomeFragment.newInstance(getString(R.string.item_person));
+                    homeFragment = HomeFragment.newInstance(getString(R.string.item_setting));
                 }
                 transaction.replace(R.id.sub_content, homeFragment);
                 break;
             case 1:
-
                 initToolbar(R.id.tl_, R.id.toolbar_title, "日程管理");
                 if (dateFragment == null) {
-                    dateFragment = DateFragment.newInstance(getString(R.string.item_person));
+                    dateFragment = DateFragment.newInstance(getString(R.string.item_setting));
                 }
                 transaction.replace(R.id.sub_content, dateFragment);
                 break;
@@ -83,17 +79,16 @@ public class MainActivity extends Utils implements BottomNavigationBar.OnTabSele
 
                 initToolbar(R.id.tl_, R.id.toolbar_title, "资金管理");
                 if (captialFragment == null) {
-                    captialFragment = CaptialFragment.newInstance(getString(R.string.item_person));
+                    captialFragment = CaptialFragment.newInstance(getString(R.string.item_setting));
                 }
                 transaction.replace(R.id.sub_content, captialFragment);
                 break;
             case 3:
                 initToolbar(R.id.tl_, R.id.toolbar_title, "设置");
                 if (settingFragment == null) {
-                    settingFragment = SettingFragment.newInstance(getString(R.string.item_person));
+                    settingFragment = SettingFragment.newInstance(getString(R.string.item_setting));
                 }
                 transaction.replace(R.id.sub_content, settingFragment);
-
                 break;
         }
         transaction.commit();
