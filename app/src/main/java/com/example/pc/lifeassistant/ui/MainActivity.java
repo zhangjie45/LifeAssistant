75b01a64d5f356922662dcbe9a7098b75b1b5af4
zhangjie <1109446910@qq.com>
2019-05-12 16:19:36 +0800

    fragment关闭右滑退出

--- app/src/main/java/com/example/pc/lifeassistant/ui/MainActivity.java
+++ app/src/main/java/com/example/pc/lifeassistant/ui/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends BaseActivity implements BottomNavigationBar.On
         setContentView(R.layout.activity_main);
         init();
         event();
+        closeSwipeBack();
 
 
     }
